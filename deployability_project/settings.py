6e007e320356db84193c09c56759e5360883bd87
abahtiti <abahtiti1@gmail.com>
2020-05-17 15:42:44 +0000

    update settings

--- deployability_project/settings.py
+++ deployability_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'nt+t*t0kd9*0=p-fmt5-z-*^lvsz$ruo45sk=&gtzhvldzg#w^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['abahtiti.pythonanywhere.com']
 #ALLOWED_HOSTS = ['*']
 
 
@@ -123,4 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 LOGIN_URL = '/login'
