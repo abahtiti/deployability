bf72b15b568571ac50ce9ceb87e18a3d90770aca
abahtiti <abahtiti1@gmail.com>
2020-05-17 19:56:06 +1000

    change files name

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bfc_euclid_deployability_project.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'deployability_project.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
